786572ec430de5afd7f494a3d00d82c00f3bf044
shu-ray <1syuraih@gmail.com>
2022-12-30 23:25:24 +0800

    import avi functions from jameszah esp32-cam-video-recorder-junior-60

--- main/main.ino
+++ main/main.ino
@@ -25,11 +25,7 @@
 #define HREF_GPIO_NUM 23
 #define PCLK_GPIO_NUM 22
 
-const char * countPath = "/camSettings/picIndex.txt";
-
-const byte wakeupPin = 12;
-const byte flashSwitch = 13;
-const byte flashPin = 4;
+char * countPath = "/camSettings/picIndex.txt";
 const byte buttonPin = 12;
 
 void configESPCamera()
@@ -144,11 +140,485 @@ String readFile(const char * path){
   return temp;;
 }
 
+class FileCount{
+  public:
+    char * countFilePat = "";
+    String countVal = "";
+    FileCount(char * countFilePath){
+      countFilePat = countFilePath;
+    }
+
+    String getCount(){
+      countVal = readFile(countFilePat);
+      return countVal;
+    }
+
+    void setNextCount(){
+      printFile(countFilePat,countVal.toInt() + 1);
+    }
+};
+
+FileCount Count(countPath);
+
+
+// In honor of jameszah's esp32-cam-video-recorder-junior-60
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Make the avi functions
+//
+//   start_avi() - open the file and write headers
+//   another_pic_avi() - write one more frame of movie
+//   end_avi() - write the final parameters and close the file
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// start_avi - open the files and write in headers
+//
+
+File avifile;
+File idxfile;
+#define AVIOFFSET 240 // AVI main header length
+static int i = 0;
+const int avi_header[AVIOFFSET] PROGMEM = {
+  0x52, 0x49, 0x46, 0x46, 0xD8, 0x01, 0x0E, 0x00, 0x41, 0x56, 0x49, 0x20, 0x4C, 0x49, 0x53, 0x54,
+  0xD0, 0x00, 0x00, 0x00, 0x68, 0x64, 0x72, 0x6C, 0x61, 0x76, 0x69, 0x68, 0x38, 0x00, 0x00, 0x00,
+  0xA0, 0x86, 0x01, 0x00, 0x80, 0x66, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00,
+  0x64, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x80, 0x02, 0x00, 0x00, 0xe0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4C, 0x49, 0x53, 0x54, 0x84, 0x00, 0x00, 0x00,
+  0x73, 0x74, 0x72, 0x6C, 0x73, 0x74, 0x72, 0x68, 0x30, 0x00, 0x00, 0x00, 0x76, 0x69, 0x64, 0x73,
+  0x4D, 0x4A, 0x50, 0x47, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x73, 0x74, 0x72, 0x66,
+  0x28, 0x00, 0x00, 0x00, 0x28, 0x00, 0x00, 0x00, 0x80, 0x02, 0x00, 0x00, 0xe0, 0x01, 0x00, 0x00,
+  0x01, 0x00, 0x18, 0x00, 0x4D, 0x4A, 0x50, 0x47, 0x00, 0x84, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x49, 0x4E, 0x46, 0x4F,
+  0x10, 0x00, 0x00, 0x00, 0x6A, 0x61, 0x6D, 0x65, 0x73, 0x7A, 0x61, 0x68, 0x61, 0x72, 0x79, 0x20,
+  0x76, 0x36, 0x30, 0x20, 0x4C, 0x49, 0x53, 0x54, 0x00, 0x01, 0x0E, 0x00, 0x6D, 0x6F, 0x76, 0x69,
+};
+int framesize = FRAMESIZE_SVGA;
+#define BUFFSIZE 512
+uint8_t buf[BUFFSIZE];
+String avi_file_name;
+
+struct frameSizeStruct {
+  uint8_t frameWidth[2];
+  uint8_t frameHeight[2];
+};
+static const frameSizeStruct frameSizeData[] = {
+  {{0x60, 0x00}, {0x60, 0x00}}, // FRAMESIZE_96X96,    // 96x96
+  {{0xA0, 0x00}, {0x78, 0x00}}, // FRAMESIZE_QQVGA,    // 160x120
+  {{0xB0, 0x00}, {0x90, 0x00}}, // FRAMESIZE_QCIF,     // 176x144
+  {{0xF0, 0x00}, {0xB0, 0x00}}, // FRAMESIZE_HQVGA,    // 240x176
+  {{0xF0, 0x00}, {0xF0, 0x00}}, // FRAMESIZE_240X240,  // 240x240
+  {{0x40, 0x01}, {0xF0, 0x00}}, // FRAMESIZE_QVGA,     // 320x240   framessize
+  {{0x90, 0x01}, {0x28, 0x01}}, // FRAMESIZE_CIF,      // 400x296       bytes per buffer required in psram - quality must be higher number (lower quality) than config quality
+  {{0xE0, 0x01}, {0x40, 0x01}}, // FRAMESIZE_HVGA,     // 480x320       low qual  med qual  high quality
+  {{0x80, 0x02}, {0xE0, 0x01}}, // FRAMESIZE_VGA,      // 640x480   8   11+   ##  6-10  ##  0-5         indoor(56,COUNT=3)  (56,COUNT=2)          (56,count=1)
+  //               38,400    61,440    153,600
+  {{0x20, 0x03}, {0x58, 0x02}}, // FRAMESIZE_SVGA,     // 800x600   9                       240,000
+  {{0x00, 0x04}, {0x00, 0x03}}, // FRAMESIZE_XGA,      // 1024x768  10
+  {{0x00, 0x05}, {0xD0, 0x02}}, // FRAMESIZE_HD,       // 1280x720  11  115,200   184,320   460,800     (11)50.000  25.4fps   (11)50.000 12fps    (11)50,000  12.7fps
+  {{0x00, 0x05}, {0x00, 0x04}}, // FRAMESIZE_SXGA,     // 1280x1024 12
+  {{0x40, 0x06}, {0xB0, 0x04}}, // FRAMESIZE_UXGA,     // 1600x1200 13  240,000   384,000   960,000
+  // 3MP Sensors
+  {{0x80, 0x07}, {0x38, 0x04}}, // FRAMESIZE_FHD,      // 1920x1080 14  259,200   414,720   1,036,800   (11)210,000 5.91fps
+  {{0xD0, 0x02}, {0x00, 0x05}}, // FRAMESIZE_P_HD,     //  720x1280 15
+  {{0x60, 0x03}, {0x00, 0x06}}, // FRAMESIZE_P_3MP,    //  864x1536 16
+  {{0x00, 0x08}, {0x00, 0x06}}, // FRAMESIZE_QXGA,     // 2048x1536 17  393,216   629,146   1,572,864
+  // 5MP Sensors
+  {{0x00, 0x0A}, {0xA0, 0x05}}, // FRAMESIZE_QHD,      // 2560x1440 18  460,800   737,280   1,843,200   (11)400,000 3.5fps    (11)330,000 1.95fps
+  {{0x00, 0x0A}, {0x40, 0x06}}, // FRAMESIZE_WQXGA,    // 2560x1600 19
+  {{0x38, 0x04}, {0x80, 0x07}}, // FRAMESIZE_P_FHD,    // 1080x1920 20
+  {{0x00, 0x0A}, {0x80, 0x07}}  // FRAMESIZE_QSXGA,    // 2560x1920 21  614,400   983,040   2,457,600   (15)425,000 3.25fps   (15)382,000 1.7fps  (15)385,000 1.7fps
+
+};
+static void inline print_quartet(unsigned long i, File fd) {
+
+  uint8_t y[4];
+  y[0] = i % 0x100;
+  y[1] = (i >> 8) % 0x100;
+  y[2] = (i >> 16) % 0x100;
+  y[3] = (i >> 24) % 0x100;
+  size_t i1_err = fd.write(y , 4);
+}
+
+int frame_interval = 0;          // record at full speed
+int speed_up_factor = 1;          // play at realtime
+int avi_length = 1800;            // how long a movie in seconds -- 1800 sec = 30 min
+uint32_t startms;
+long totalp;
+long totalw;
+
+unsigned long movi_size = 0;
+unsigned long jpeg_size = 0;
+unsigned long idx_offset = 0;
+uint32_t uVideoLen = 0;
+
+int bad_jpg = 0;
+int extend_jpg = 0;
+int normal_jpg = 0;
+
+long time_in_loop = 0;
+long time_in_camera = 0;
+long time_in_sd = 0;
+long time_in_good = 0;
+long time_total = 0;
+long time_in_web1 = 0;
+long time_in_web2 = 0;
+long delay_wait_for_sd = 0;
+long wait_for_cam = 0;
+
+uint16_t remnant = 0;
+#define fbs 8 // was 64 -- how many kb of static ram for psram -> sram buffer for sd write
+uint8_t framebuffer_static[fbs * 1024 + 20];
+static void inline print_2quartet(unsigned long i, unsigned long j, File fd) {
+
+  uint8_t y[8];
+  y[0] = i % 0x100;
+  y[1] = (i >> 8) % 0x100;
+  y[2] = (i >> 16) % 0x100;
+  y[3] = (i >> 24) % 0x100;
+  y[4] = j % 0x100;
+  y[5] = (j >> 8) % 0x100;
+  y[6] = (j >> 16) % 0x100;
+  y[7] = (j >> 24) % 0x100;
+  size_t i1_err = fd.write(y , 8);
+}
+
+int do_it_now = 0;
+uint16_t frame_cnt = 0;
+uint32_t elapsedms;
+
+uint8_t zero_buf[4] = {0x00, 0x00, 0x00, 0x00};
+uint8_t dc_buf[4] = {0x30, 0x30, 0x64, 0x63};    // "00dc"
+uint8_t dc_and_zero_buf[8] = {0x30, 0x30, 0x64, 0x63, 0x00, 0x00, 0x00, 0x00};
+
+uint8_t avi1_buf[4] = {0x41, 0x56, 0x49, 0x31};    // "AVI1"
+uint8_t idx1_buf[4] = {0x69, 0x64, 0x78, 0x31};    // "idx1"
+
+int gframe_cnt;
+int gfblen;
+int gj;
+int  gmdelay;
+
+
+static void start_avi() {
+
+  long start = millis();
+
+  Serial.println("Starting an avi ");
+
+  avi_file_name = "avi_" + Count.getCount() + ".avi";
+
+  avifile = SD_MMC.open(avi_file_name, "w");
+  idxfile = SD_MMC.open("/idx.tmp", "w");
+
+  if (avifile) {
+    Serial.printf("File open: %s\n", avi_file_name);
+  }  else  {
+    Serial.println("Could not open file");
+    return;
+  }
+
+  if (idxfile)  {
+    //Serial.printf("File open: %s\n", "//idx.tmp");
+  }  else  {
+    Serial.println("Could not open file /idx.tmp");
+    return;
+  }
+
+  for ( i = 0; i < AVIOFFSET; i++) {
+    char ch = pgm_read_byte(&avi_header[i]);
+    buf[i] = ch;
+  }
+
+  memcpy(buf + 0x40, frameSizeData[framesize].frameWidth, 2);
+  memcpy(buf + 0xA8, frameSizeData[framesize].frameWidth, 2);
+  memcpy(buf + 0x44, frameSizeData[framesize].frameHeight, 2);
+  memcpy(buf + 0xAC, frameSizeData[framesize].frameHeight, 2);
+
+  size_t err = avifile.write(buf, AVIOFFSET);
+
+  uint8_t ex_fps = 1;
+  if (frame_interval == 0) {
+    if (framesize >= 11) {
+      ex_fps = 12.5 * speed_up_factor ;;
+    } else {
+      ex_fps = 25.0 * speed_up_factor;
+    }
+  } else {
+    ex_fps = round(1000.0 / frame_interval * speed_up_factor);
+  }
+
+  avifile.seek( 0x84 , SeekSet);
+  print_quartet((int)ex_fps, avifile);
+
+  avifile.seek( AVIOFFSET, SeekSet);
+
+  Serial.print(F("\nRecording "));
+  Serial.print(avi_length);
+  Serial.println(" seconds.");
+
+  startms = millis();
+
+  totalp = 0;
+  totalw = 0;
+
+  jpeg_size = 0;
+  movi_size = 0;
+  uVideoLen = 0;
+  idx_offset = 4;
+
+  bad_jpg = 0;
+  extend_jpg = 0;
+  normal_jpg = 0;
+
+  time_in_loop = 0;
+  time_in_camera = 0;
+  time_in_sd = 0;
+  time_in_good = 0;
+  time_total = 0;
+  time_in_web1 = 0;
+  time_in_web2 = 0;
+  delay_wait_for_sd = 0;
+  wait_for_cam = 0;
+
+  time_in_sd += (millis() - start);
+
+  avifile.flush();
+
+} // end of start avi
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//  another_save_avi saves another frame to the avi file, uodates index
+//           -- pass in a fb pointer to the frame to add
+//
+
+static void another_save_avi(camera_fb_t * fb ) {
+
+  long start = millis();
+
+  int fblen;
+  fblen = fb->len;
+
+  int fb_block_length;
+  uint8_t* fb_block_start;
+
+  jpeg_size = fblen;
+
+  remnant = (4 - (jpeg_size & 0x00000003)) & 0x00000003;
+
+  long bw = millis();
+  long frame_write_start = millis();
+
+  framebuffer_static[0] = 0x30;       // "00dc"
+  framebuffer_static[1] = 0x30;
+  framebuffer_static[2] = 0x64;
+  framebuffer_static[3] = 0x63;
+
+  int jpeg_size_rem = jpeg_size + remnant;
+
+  framebuffer_static[4] = jpeg_size_rem % 0x100;
+  framebuffer_static[5] = (jpeg_size_rem >> 8) % 0x100;
+  framebuffer_static[6] = (jpeg_size_rem >> 16) % 0x100;
+  framebuffer_static[7] = (jpeg_size_rem >> 24) % 0x100;
+
+  fb_block_start = fb->buf;
+
+  if (fblen > fbs * 1024 - 8 ) {                     // fbs is the size of frame buffer static
+    fb_block_length = fbs * 1024;
+    fblen = fblen - (fbs * 1024 - 8);
+    memcpy(framebuffer_static + 8, fb_block_start, fb_block_length - 8);
+    fb_block_start = fb_block_start + fb_block_length - 8;
+
+  } else {
+    fb_block_length = fblen + 8  + remnant;
+    memcpy(framebuffer_static + 8, fb_block_start,  fblen);
+    fblen = 0;
+  }
+
+  size_t err = avifile.write(framebuffer_static, fb_block_length);
+
+  if (err != fb_block_length) {
+    Serial.print("Error on avi write: err = "); Serial.print(err);
+    Serial.print(" len = "); Serial.println(fb_block_length);
+  }
+
+  while (fblen > 0) {
+
+    if (fblen > fbs * 1024) {
+      fb_block_length = fbs * 1024;
+      fblen = fblen - fb_block_length;
+    } else {
+      fb_block_length = fblen  + remnant;
+      fblen = 0;
+    }
+
+    memcpy(framebuffer_static, fb_block_start, fb_block_length);
+
+    size_t err = avifile.write(framebuffer_static,  fb_block_length);
+
+    if (err != fb_block_length) {
+      Serial.print("Error on avi write: err = "); Serial.print(err);
+      Serial.print(" len = "); Serial.println(fb_block_length);
+    }
+
+    fb_block_start = fb_block_start + fb_block_length;
+    delay(0);
+  }
+
+
+  movi_size += jpeg_size;
+  uVideoLen += jpeg_size;
+  long frame_write_end = millis();
+
+  print_2quartet(idx_offset, jpeg_size, idxfile);
+
+  idx_offset = idx_offset + jpeg_size + remnant + 8;
+
+  movi_size = movi_size + remnant;
+
+  if ( do_it_now == 1 && frame_cnt < 1011) {
+    do_it_now = 0;
+    Serial.printf("Frame %6d, len %6d, extra  %4d, cam time %7d,  sd time %4d -- \n", gframe_cnt, gfblen, gj - 1, gmdelay / 1000, millis() - bw);
+  }
+
+  totalw = totalw + millis() - bw;
+  time_in_sd += (millis() - start);
+
+  avifile.flush();
+
+
+} // end of another_pic_avi
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//  end_avi writes the index, and closes the files
+//
+
+static void end_avi() {
+
+  long start = millis();
+
+  unsigned long current_end = avifile.position();
+
+  Serial.println("End of avi - closing the files");
+
+  if (frame_cnt <  5 ) {
+    Serial.println("Recording screwed up, less than 5 frames, forget index\n");
+    idxfile.close();
+    avifile.close();
+    int xx = remove("/idx.tmp");
+    int yy = remove(avi_file_name.c_str());
+
+  } else {
+
+    elapsedms = millis() - startms;
+
+    float fRealFPS = (1000.0f * (float)frame_cnt) / ((float)elapsedms) * speed_up_factor;
+
+    float fmicroseconds_per_frame = 1000000.0f / fRealFPS;
+    uint8_t iAttainedFPS = round(fRealFPS) ;
+    uint32_t us_per_frame = round(fmicroseconds_per_frame);
+
+    //Modify the MJPEG header from the beginning of the file, overwriting various placeholders
+
+    avifile.seek( 4 , SeekSet);
+    print_quartet(movi_size + 240 + 16 * frame_cnt + 8 * frame_cnt, avifile);
+
+    avifile.seek( 0x20 , SeekSet);
+    print_quartet(us_per_frame, avifile);
+
+    unsigned long max_bytes_per_sec = (1.0f * movi_size * iAttainedFPS) / frame_cnt;
+
+    avifile.seek( 0x24 , SeekSet);
+    print_quartet(max_bytes_per_sec, avifile);
+
+    avifile.seek( 0x30 , SeekSet);
+    print_quartet(frame_cnt, avifile);
+
+    avifile.seek( 0x8c , SeekSet);
+    print_quartet(frame_cnt, avifile);
+
+    avifile.seek( 0x84 , SeekSet);
+    print_quartet((int)iAttainedFPS, avifile);
+
+    avifile.seek( 0xe8 , SeekSet);
+    print_quartet(movi_size + frame_cnt * 8 + 4, avifile);
+
+    Serial.println(F("\n*** Video recorded and saved ***\n"));
+
+    Serial.printf("Recorded %5d frames in %5d seconds\n", frame_cnt, elapsedms / 1000);
+    Serial.printf("File size is %u bytes\n", movi_size + 12 * frame_cnt + 4);
+    Serial.printf("Adjusted FPS is %5.2f\n", fRealFPS);
+    Serial.printf("Max data rate is %lu bytes/s\n", max_bytes_per_sec);
+    Serial.printf("Frame duration is %d us\n", us_per_frame);
+    Serial.printf("Average frame length is %d bytes\n", uVideoLen / frame_cnt);
+    Serial.print("Average picture time (ms) "); Serial.println( 1.0 * totalp / frame_cnt);
+    Serial.print("Average write time (ms)   "); Serial.println( 1.0 * totalw / frame_cnt );
+    Serial.print("Normal jpg % ");  Serial.println( 100.0 * normal_jpg / frame_cnt, 1 );
+    Serial.print("Extend jpg % ");  Serial.println( 100.0 * extend_jpg / frame_cnt, 1 );
+    Serial.print("Bad    jpg % ");  Serial.println( 100.0 * bad_jpg / frame_cnt, 5 );
+
+    Serial.printf("Writng the index, %d frames\n", frame_cnt);
+
+    avifile.seek( current_end , SeekSet);
+
+    idxfile.close();
+
+    size_t i1_err = avifile.write(idx1_buf, 4);
+
+    print_quartet(frame_cnt * 16, avifile);
+
+    idxfile = SD_MMC.open("/idx.tmp", "r");
+
+    if (idxfile)  {
+    }  else  {
+      Serial.println("Could not open index file");
+      return;
+    }
+
+    char * AteBytes;
+    AteBytes = (char*) malloc (8);
+
+    for (int i = 0; i < frame_cnt; i++) {
+      size_t res = idxfile.readBytes( AteBytes, 8);
+      size_t i1_err = avifile.write(dc_buf, 4);
+      size_t i2_err = avifile.write(zero_buf, 4);
+      size_t i3_err = avifile.write((uint8_t *)AteBytes, 8);
+    }
+
+    free(AteBytes);
+
+    idxfile.close();
+    avifile.close();
+
+    int xx = SD_MMC.remove("/idx.tmp");
+  }
+
+  Serial.println("---");
+  time_in_sd += (millis() - start);
+
+  Serial.println("");
+  time_total = millis() - startms;
+  Serial.printf("waiting for cam %10dms, %4.1f%%\n", wait_for_cam , 100.0 * wait_for_cam  / time_total);
+  Serial.printf("Time in camera  %10dms, %4.1f%%\n", time_in_camera, 100.0 * time_in_camera / time_total);
+  Serial.printf("waiting for sd  %10dms, %4.1f%%\n", delay_wait_for_sd , 100.0 * delay_wait_for_sd  / time_total);
+  Serial.printf("Time in sd      %10dms, %4.1f%%\n", time_in_sd    , 100.0 * time_in_sd     / time_total);
+  Serial.printf("web (core 1)    %10dms, %4.1f%%\n", time_in_web1  , 100.0 * time_in_web1   / time_total);
+  Serial.printf("web (core 0)    %10dms, %4.1f%%\n", time_in_web2  , 100.0 * time_in_web2   / time_total);
+  Serial.printf("time total      %10dms, %4.1f%%\n", time_total    , 100.0 * time_total     / time_total);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 void takeNewPhoto(){
 
   // Path where new picture will be saved in SD Card
-  String count = readFile(countPath);
-  String path = "/image" + count + ".jpg";
+  String path = "/image" + Count.getCount(); + ".jpg";
   
   // Save picture to microSD card
   fs::FS &fs = SD_MMC;
@@ -160,10 +630,8 @@ void takeNewPhoto(){
 
   // Setup frame buffer
   camera_fb_t *fb = esp_camera_fb_get();
-
   if (!fb){
     Serial.println("Camera capture failed");
-//    digitalWrite(flashPin,LOW);
     return;
   }
 
@@ -174,10 +642,12 @@ void takeNewPhoto(){
 
   // Return the frame buffer back to the driver for reuse
   esp_camera_fb_return(fb);
-  printFile(countPath,count.toInt() + 1);
+  // Update picture number counter
+  Count.setNextCount();
 
   Serial.printf("Saved file to path: %s\n", path.c_str());
 }
+
 
 // the camera require few image capture to calibrate exposure etc
 void camCalibrate(){
@@ -189,7 +659,6 @@ void camCalibrate(){
     if (now % 120 == 0){
       count++;
       camera_fb_t *fb = esp_camera_fb_get();
-      //delayMicroseconds(500);
       if (!fb){
         Serial.println("Cab failed");
         return;
@@ -198,7 +667,6 @@ void camCalibrate(){
       esp_camera_fb_return(fb);      
       Serial.print(count);
     }
-    //delayMicroseconds(500);
     delay(1);
   }
   Serial.println("\nCab done");
